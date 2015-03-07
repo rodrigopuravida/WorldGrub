7922be577de83f1acdef51d2a8736c78a29c17bb
Rodrigo Carballo <rodrigo.puravida@gmail.com>
2015-03-07 15:50:39 -0800

    Added BurgerController

---
+++ WorldGrub/Controllers/BurgerContainerControllerViewController.h
@@ -0,0 +1,13 @@
+//
+//  BurgerContainerControllerViewController.h
+//  WorldGrub
+//
+//  Created by Rodrigo Carballo on 3/7/15.
+//  Copyright (c) 2015 Rodrigo Carballo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BurgerContainerControllerViewController : UIViewController
+
+@end
