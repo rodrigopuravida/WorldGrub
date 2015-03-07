fd28217fa9eb9b6d9af6f764579cec001929d213
Rodrigo Carballo <rodrigo.puravida@gmail.com>
2015-03-07 14:01:09 -0800

    Initial Commit

---
+++ WorldGrub/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  WorldGrub
+//
+//  Created by Rodrigo Carballo on 3/7/15.
+//  Copyright (c) 2015 Rodrigo Carballo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ WorldGrub/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WorldGrub
+//
+//  Created by Rodrigo Carballo on 3/7/15.
+//  Copyright (c) 2015 Rodrigo Carballo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
