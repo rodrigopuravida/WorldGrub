9ea65e2311cc7d6a4bb6bd4029e21b047be62ebc
Rodrigo Carballo <rodrigo.puravida@gmail.com>
2015-03-09 20:51:42 -0700

    Able to propagate cels in RecipeListViewController - test label

--- RecipeListViewController.m
+++ RecipeListViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "RecipeListViewController.h"
+#import "RecipeCell.h"
 
-@interface RecipeListViewController ()
+@interface RecipeListViewController () <UITableViewDataSource>
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 @end
 
@@ -16,6 +19,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.tableView.dataSource = self;
+    
     // Do any additional setup after loading the view.
 }
 
@@ -23,6 +28,21 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 2;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    RecipeCell *cell = [tableView dequeueReusableCellWithIdentifier:@"RECIPE_CELL"
+                                                         forIndexPath:indexPath];
+//    cell.avatarImageView.image = nil;
+//    Question *question = self.questions[indexPath.row];
+//    cell.titleTextView.text = question.title;
+    //lazy loading of image
+        return cell;
+}
+
 
 /*
 #pragma mark - Navigation
