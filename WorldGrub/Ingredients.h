a042fe6590ecce80d5f3b1ee80ec2eb013a0d648
Rodrigo Carballo <rodrigo.puravida@gmail.com>
2015-05-13 14:28:27 -0700

    Created ingredients class

---
+++ WorldGrub/Ingredients.h
@@ -0,0 +1,13 @@
+//
+//  Ingredients.h
+//  WorldGrub
+//
+//  Created by Rodrigo Carballo on 5/13/15.
+//  Copyright (c) 2015 Rodrigo Carballo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Ingredients : NSObject
+
+@end
---
+++ WorldGrub/Ingredients.m
@@ -0,0 +1,13 @@
+//
+//  Ingredients.m
+//  WorldGrub
+//
+//  Created by Rodrigo Carballo on 5/13/15.
+//  Copyright (c) 2015 Rodrigo Carballo. All rights reserved.
+//
+
+#import "Ingredients.h"
+
+@implementation Ingredients
+
+@end
